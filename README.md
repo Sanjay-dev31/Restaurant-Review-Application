e3eff5adb1ee3f0c5a32ecf0dda3bce1065622ad
Sanjay P <sanjay.badharayan@gmail.com>
2020-01-15 00:45:21 +1100

    For Review

---
+++ README.md
@@ -0,0 +1,15 @@
+# Restaurant Review App
+
+Restaurant review app provides review feedback on few of the restaurants in USA.
+This app is enabled to work on offline mode as well, after the initial load.
+
+## Installation
+To install and run this app:
+1. Download or clone this repo.
+2. Run a local server preferably Python
+3. Kick start Python server with the following command:
+    Python 2: python -m SimpleHTTPServer 8000
+    Python 3: python3 -m http.server 8000
+    Visit the site in your browser at http://localhost:8000
+4. Enjoy the review app!!!
+
---
+++ js/main.js
@@ -0,0 +1,223 @@
+let restaurants,
+    neighborhoods,
+    cuisines;
+var newMap;
+let markers = [];
+
+/**
+ * Fetch neighborhoods and cuisines as soon as the page is loaded.
+ */
+document.addEventListener('DOMContentLoaded', (event) => {
+    initMap(); // added
+    fetchNeighborhoods();
+    fetchCuisines();
+});
+
+/**
+ * Fetch all neighborhoods and set their HTML.
+ */
+fetchNeighborhoods = () => {
+    DBHelper.fetchNeighborhoods((error, neighborhoods) => {
+        if (error) { // Got an error
+            console.error(error);
+        } else {
+            self.neighborhoods = neighborhoods;
+            fillNeighborhoodsHTML();
+        }
+    });
+}
+
+/**
+ * Set neighborhoods HTML.
+ */
+fillNeighborhoodsHTML = (neighborhoods = self.neighborhoods) => {
+    const select = document.getElementById('neighborhoods-select');
+    neighborhoods.forEach(neighborhood => {
+        const option = document.createElement('option');
+        option.innerHTML = neighborhood;
+        option.value = neighborhood;
+        select.append(option);
+    });
+}
+
+/**
+ * Fetch all cuisines and set their HTML.
+ */
+fetchCuisines = () => {
+    DBHelper.fetchCuisines((error, cuisines) => {
+        if (error) { // Got an error!
+            console.error(error);
+        } else {
+            self.cuisines = cuisines;
+            fillCuisinesHTML();
+        }
+    });
+}
+
+/**
+ * Set cuisines HTML.
+ */
+fillCuisinesHTML = (cuisines = self.cuisines) => {
+    const select = document.getElementById('cuisines-select');
+
+    cuisines.forEach(cuisine => {
+        const option = document.createElement('option');
+        option.innerHTML = cuisine;
+        option.value = cuisine;
+        select.append(option);
+    });
+}
+
+/**
+ * Initialize leaflet map, called from HTML.
+ */
+initMap = () => {
+    self.newMap = L.map('map', {
+        center: [40.722216, -73.987501],
+        zoom: 12,
+        scrollWheelZoom: false
+    });
+    L.tileLayer(
+        'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+            mapboxToken: 'pk.eyJ1Ijoia2F0ZnJvZyIsImEiOiJjamxnd3RlZzEwcWhxM2twa2gyOGRldjVqIn0.8Arlw10qwQvLPIcw5UU0KQ',
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: 'mapbox.streets'
+        }).addTo(newMap);
+
+    updateRestaurants();
+}
+
+/**
+ * Update page and map for current restaurants.
+ */
+updateRestaurants = () => {
+    const cSelect = document.getElementById('cuisines-select');
+    const nSelect = document.getElementById('neighborhoods-select');
+
+    const cIndex = cSelect.selectedIndex;
+    const nIndex = nSelect.selectedIndex;
+
+    const cuisine = cSelect[cIndex].value;
+    const neighborhood = nSelect[nIndex].value;
+
+    DBHelper.fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood,
+        (error, restaurants) => {
+            if (error) { // Got an error!
+                console.error(error);
+            } else {
+                resetRestaurants(restaurants);
+                fillRestaurantsHTML();
+            }
+        })
+}
+
+/**
+ * Clear current restaurants, their HTML and remove their map markers.
+ */
+resetRestaurants = (restaurants) => {
+    // Remove all restaurants
+    self.restaurants = [];
+    const ul = document.getElementById('restaurants-list');
+    ul.innerHTML = '';
+
+    // Remove all map markers
+    if (self.markers) {
+        self.markers.forEach(marker => marker.remove());
+    }
+    self.markers = [];
+    self.restaurants = restaurants;
+}
+
+/**
+ * Create all restaurants HTML and add them to the webpage.
+ */
+fillRestaurantsHTML = (restaurants = self.restaurants) => {
+    const ul = document.getElementById('restaurants-list');
+    restaurants.forEach(restaurant => {
+        ul.append(createRestaurantHTML(restaurant));
+    });
+    addMarkersToMap();
+}
+
+/**
+ * Create restaurant HTML.
+ */
+createRestaurantHTML = (restaurant) => {
+    const li = document.createElement('li');
+    li.setAttribute("role", "listitem");
+    li.className = "restaurant-listing";
+
+    const image = document.createElement('img');
+    image.className = 'restaurant-img';
+    image.tabIndex = 0;
+    image.src = DBHelper.imageUrlForRestaurant(restaurant);
+    image.alt = DBHelper.imageAltForRestaurant(restaurant);
+    li.append(image);
+
+    const div = document.createElement('div');
+    div.className = 'restaurant-text-area';
+    li.append(div);
+
+    const name = document.createElement('h1');
+    name.className = 'restaurant-name';
+    name.innerHTML = restaurant.name;
+    name.tabIndex = 0;
+    div.append(name);
+
+    const address = document.createElement('p');
+    address.innerHTML = restaurant.address;
+    address.tabIndex = 0;
+    div.append(address);
+
+    const neighborhood = document.createElement('p');
+    neighborhood.innerHTML = `<strong>Neighborhood:</strong>  ${restaurant.neighborhood}`;
+    neighborhood.tabIndex = 0;
+    neighborhood.setAttribute('role', 'note');
+    neighborhood.setAttribute('aria-roledescription', 'Neighborhood of Restaurant');
+    div.append(neighborhood);
+
+    const cuisine = document.createElement('p');
+    cuisine.innerHTML = `<strong>Cuisine</strong>:  ${restaurant.cuisine_type}`;
+    cuisine.setAttribute('role', 'note');
+    cuisine.setAttribute('aria-roledescription', 'Type of Cuisine');
+    cuisine.tabIndex = 0;
+    div.append(cuisine);
+
+    const more = document.createElement('a');
+    more.innerHTML = 'View Details';
+    more.href = DBHelper.urlForRestaurant(restaurant);
+    div.append(more)
+
+    return li
+}
+
+/**
+ * Add markers for current restaurants to the map.
+ */
+addMarkersToMap = (restaurants = self.restaurants) => {
+    restaurants.forEach(restaurant => {
+        // Add marker to the map
+        const marker = DBHelper.mapMarkerForRestaurant(restaurant,
+            self.newMap);
+        marker.on("click", onClick);
+
+        function onClick() {
+            window.location.href = marker.options.url;
+        }
+        self.markers.push(marker);
+    });
+
+}
+/* addMarkersToMap = (restaurants = self.restaurants) => {
+  restaurants.forEach(restaurant => {
+    // Add marker to the map
+    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
+    google.maps.event.addListener(marker, 'click', () => {
+      window.location.href = marker.url
+    });
+    self.markers.push(marker);
+  });
+} */
